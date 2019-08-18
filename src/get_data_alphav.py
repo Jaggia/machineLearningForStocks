c0797a16cb8146c52e2668e4614fe47ff6b26bfd
Dillon <dillon.notz@gmail.com>
2019-08-18 14:46:37 -0400

    Add function arguments for plotting and saving to CSV; pretty-prints meta_data

--- src/get_data_alphav.py
+++ src/get_data_alphav.py
@@ -6,6 +6,7 @@ from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 import matplotlib.pyplot as plt
 from sys import platform as sys_pf
+from pprint import pprint
 if sys_pf == 'darwin':
     import matplotlib
     matplotlib.use("TkAgg")
@@ -22,33 +23,50 @@ def convert_to_unix(date):
 
     return int(mktime(datum.timetuple()))
 
-def getPrices(stocks):
+
+def getPrices(stocks, plot=False, write_to_csv=False):
     for stock in stocks:
         ts = TimeSeries(key='UOVDY8C1SRAQHY5D', output_format='pandas')
         data, meta_data = ts.get_intraday(symbol=stock, interval='1min', outputsize='full')
         # data, meta_data = ts.get_daily(symbol='DIS', outputsize='full')
-        data['4. close'].plot()
-        # plt.title('Intraday Times Series for the MSFT stock (1 min)')
-        plt.title('Daily Times Series for the ', stock, ' stock (1 min)')
-        plt.show()
 
-def getIndicators(stocks):
+        pprint(meta_data)
+
+        if plot:
+            data['4. close'].plot()
+            plt.title('Prices for the' + stock + 'stock (1 min)')
+            plt.xticks(rotation=15)  # Help prevent axis labels from overlapping
+            plt.show()
+
+        if write_to_csv:
+            csv_data = pd.DataFrame(data)
+            csv_data.to_csv("../prices_data/" + stock + "/full.csv", index=False)
+
+
+def getIndicators(stocks, plot=False, write_to_csv=False):
     for stock in stocks:
         ti = TechIndicators(key='UOVDY8C1SRAQHY5D', output_format='pandas')
         data, meta_data = ti.get_ema(symbol=stock, interval='60min', time_period=20)
-        # data.plot()
-        # plt.title(('ema indicator for ', stock, ' stock (60 min)'))
-        # plt.show()
-        csvData = pd.DataFrame(data)
-        csvData.to_csv("../indicators_data/EMA/" + stock + ".csv", index=False)
+
+        pprint(meta_data)
+
+        if plot:
+            data.plot()
+            plt.title('Ema Indicator for the ' + stock + ' stock (60 min)')
+            plt.xticks(rotation=15)  # Help prevent axis labels from overlapping
+            plt.show()
+
+        if write_to_csv:
+            csv_data = pd.DataFrame(data)
+            csv_data.to_csv("../indicators_data/EMA/" + stock + ".csv", index=False)
 
 
 if __name__ == "__main__":
-    # stocks = ['IBM', 'GOOG', 'AAPL', 'SPY', 'MJ']
-    stocks = ['XLF']
+    stocks = ['IBM', 'GOOG', 'AAPL', 'SPY', 'MJ']
+    # stocks = ['XLF']
     cols = []
 
-    # getPrices(stocks)
-    getIndicators(stocks)
+    # getPrices(stocks, plot=True, write_to_csv=False)
+    getIndicators(stocks, plot=True, write_to_csv=False)
 
 
